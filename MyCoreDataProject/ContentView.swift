65c5269d668271414843b355688a9b7a46d3148c
Dan Payne <dpayne@Dans-MacBook-Pro.local>
2022-03-03 18:57:53 -0700

    Initial Commit

---
+++ MyCoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCoreDataProject
+//
+//  Created by Dan Payne on 3/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCoreDataProject/MyCoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataProjectApp.swift
+//  MyCoreDataProject
+//
+//  Created by Dan Payne on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
